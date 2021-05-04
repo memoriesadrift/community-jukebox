4b6d5639c59948547b6fdef948fc2dd37fab4f29
Massimiliano Bellan <a12043516@unet.univie.ac.at>
2021-05-04 20:02:58 +0200

    Changes

---
+++ server.js
@@ -0,0 +1,45 @@
+const { Socket } = require("dgram");
+
+const httpServer = require("http").createServer();
+const io = require('socket.io')(httpServer, {
+    cors: {
+        origin: '*',
+    }
+});
+
+var users = 0;
+var currentSong = 'dQw4w9WgXcQ';
+
+io.on('connection', (client) => {
+    
+    // A user connects, returns current song and increases users number
+    console.log('User has connected');
+    users++;
+    console.log('There are ' + users + ' users');
+    io.emit('getSong', currentSong);
+    if(users == 1) {
+        io.emit('changeAdmin');
+    }
+    
+
+    // A user disconnects, decreases users number
+    client.on('disconnect', () => {
+        console.log('User has disconnected');
+        users--;
+        console.log('There are ' + users + ' users');
+    });
+
+    client.on('currentSong', () => {
+        console.log('Getting current song');
+        io.emit('currentSong', currentSong);
+    });
+
+    client.on('nextSong', (song) => {
+        currentSong = song;
+        io.emit('changeSong', currentSong);
+    });
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,13 +1,68 @@
 import React from "react";
-const VideoPlayer = () => {
-  return (
-    <div className="aspect-w-16 aspect-h-9">
-      <iframe
-        src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=0"
-        title="Video Player"
-      ></iframe>
-    </div>
-  );
+import YouTube from "react-youtube";
+import {socket} from "../apis/socketServer";
+
+var song = '';
+
+class VideoPlayer extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {song: "url", isAdmin: false, xyz: 0};
+    this._onReady = this._onReady.bind(this);
+    this.onYoutubeStateChange = this.onYoutubeStateChange.bind(this);
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  componentDidMount(){
+    //let newSong = fetchSong();
+    //this.setState({song: newSong});
+    socket.emit('currentSong');
+    socket.on('currentSong', (song) => {
+        this.setState({song: song});
+    });
+    console.log('song ' + this.state.song);
+    socket.on('changeAdmin', () => {
+      console.log('Admin change');
+      this.setState({isAdmin: true});
+    });
+    socket.on('changeSong', (song) => {
+      this.setState({song: song});
+    });
+  }
+
+  onYoutubeStateChange(event) {
+    if(!this.state.isAdmin){
+      return
+    }
+    if(event.data !== 0){
+      return
+    }
+    socket.emit('nextSong', '1vrEljMfXYo');
+    this.setState({xyz: Math.random()*12});
+  }
+
+  render () {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        autoplay: 1,
+      }
+    }
+
+    return (
+      <div className="aspect-w-16 aspect-h-9" id="player">
+        <YouTube videoId={this.state.song} opts={opts} onReady={this._onReady} onStateChange={this.onYoutubeStateChange}/>
+      </div>
+    )
+  } 
 };
+
+
 
 export default VideoPlayer;
